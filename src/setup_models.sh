340bc9c833b2d6440093294df25ded0623e54070
Osama Saleh <87075455+Elkapeer@users.noreply.github.com>
2025-06-29 01:46:00 +0300

    bug fixes

--- src/setup_models.sh
+++ src/setup_models.sh
@@ -4,6 +4,7 @@
 python -c "from huggingface_hub import snapshot_download; snapshot_download(repo_id='sentence-transformers/all-MiniLM-L6-v2', local_dir='models/embedding_model')"
 
 # Download and save spaCy model
+python -m spacy download en_core_web_sm
 python -c "
 import spacy
 nlp = spacy.load('en_core_web_sm')
